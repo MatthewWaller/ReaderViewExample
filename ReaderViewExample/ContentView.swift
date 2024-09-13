90a63243e6d9fba61130a9f38e535513b4da3c8a
Matthew Waller <hello@cephalopod.studio>
2024-09-13 10:57:00 -0500

    Initial Commit

---
+++ ReaderViewExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReaderViewExample
+//
+//  Created by Matthew Waller on 9/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReaderViewExample/ReaderViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderViewExampleApp.swift
+//  ReaderViewExample
+//
+//  Created by Matthew Waller on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ReaderViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
